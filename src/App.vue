299e1897856ffd52a1f4d2417dfd84a663837586
Mysther78 <mysther78@gmail.com>
2020-06-08 23:26:20 +0200

    RecordTable almost finish

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <RecordTable v-bind:records="records"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import RecordTable from './components/RecordTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    RecordTable
+  },
+  data() {
+    return {
+      records: []
+    }
+  },
+  mounted() {
+    this.getRecords()
+  },
+  methods: {
+    async getRecords() {
+      try {
+        const response = await fetch('https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=localisation-et-etat-des-projets-du-budget-participatif')
+        const data = await response.json()
+        this.records = data.records
+      } catch (error) {
+        console.error(error)
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/RecordTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="table">
+    <table>
+      <thead>
+        <tr>
+          <th>Libellé</th>
+          <th>Description</th>
+          <th>Realisé</th>
+          <th>Année budget</th>
+          <th>Lien</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="record in records" :key="record.recordid">
+          <td>{{ record.fields.libelle }}</td>
+          <td>{{ record.fields.description }}</td>
+          <td>{{ record.fields.realise }}</td>
+          <td>{{ record.fields.annee_budget }}</td>
+          <td> <a v-bind:href=record.fields.lien_page_projet> {{ record.fields.lien_page_projet }} </a></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MyTable',
+    props: {
+      records: Array,
+  },
+  }
+</script>
+
+<style scoped>
+  tr:nth-child(2n), th {
+    background: #EEE;
+  }
+  /* tr:nth-child(2n) {
+    background: red;
+  } */
+</style>
